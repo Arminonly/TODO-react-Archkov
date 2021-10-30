c1596001ab908205f8d2017c7fb350036c5b89dd
Arminonly <amiaby2016@gmail.com>
2021-10-30 18:56:47 +0300

    Updates

---
+++ precache-manifest.1e1c6f2fe8c16a9b5721df965aceacc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e00e916e493e75e39c1af1159c5e2e0",
+    "url": "/todo-react-archkov/index.html"
+  },
+  {
+    "revision": "3dc7808d07b8e5a22561",
+    "url": "/todo-react-archkov/static/css/main.f4903a8f.chunk.css"
+  },
+  {
+    "revision": "f09b246faf15a1ff0037",
+    "url": "/todo-react-archkov/static/js/2.85a581b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-react-archkov/static/js/2.85a581b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc7808d07b8e5a22561",
+    "url": "/todo-react-archkov/static/js/main.3978b69f.chunk.js"
+  },
+  {
+    "revision": "564c6aa5451116395db6",
+    "url": "/todo-react-archkov/static/js/runtime-main.904501db.js"
+  }
+]);
